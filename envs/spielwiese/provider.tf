991d80b1216d6dc0c37b2485d9aeb7734484f0c9
Stefan Steinert <ststefa@heldenzeit.net>
2019-07-12 10:12:02 +0200

    use clouds.yaml

--- envs/spielwiese/provider.tf
+++ envs/spielwiese/provider.tf
@@ -1,8 +1,12 @@
 provider "openstack" {
-  user_name   = var.username
-  password    = var.password
   domain_name = "tsch_rz_t_001"
-  tenant_name = "eu-ch"
+  #tenant_name = "eu-ch"
+  #user_name   = var.username
+  #password    = var.password
+  # use openstack cloud config (~/.config/openstack/clouds.yaml) instead of username/password
+  # see
+  # https://docs.openstack.org/python-openstackclient/stein/configuration/index.html
+  cloud = "otc-sbb-t"
   auth_url    = "https://iam.eu-ch.o13bb.otc.t-systems.com/v3"
 }
 
