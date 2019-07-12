df14a91a650c4b4010d2380d126a113047dddbb0
Stefan Steinert <ststefa@heldenzeit.net>
2019-07-12 10:48:46 +0200

    rename

--- modules/indexer/output.tf
+++ modules/indexer/output.tf
@@ -2,7 +2,7 @@ output "ip" {
   description = "access ip"
   value       = openstack_compute_instance_v2.indexer.access_ip_v4
 }
-output "attach" {
+output "opt_attach" {
   description = "attach.opt"
   value       = openstack_compute_volume_attach_v2.opt_attach.device
 }
