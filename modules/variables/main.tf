fd0d89eb8786bd3d6be6aa375c389d1fb69088e6
Stefan Steinert <ststefa@heldenzeit.net>
2019-07-12 10:49:03 +0200

    adds searchhead

--- modules/variables/main.tf
+++ modules/variables/main.tf
@@ -7,6 +7,7 @@ variable stage_map {
   type = "map"
   default = {
     spielwiese     = "spielwiese"
+    spielwiese-p   = "spielwiese-p"
   }
 }
 
@@ -18,11 +19,12 @@ variable "stage_letter_map" {
   description = "Each stage must be represented by a single letter"
   type = "map"
   default = {
-    spielwiese  = "S"
-    production  = "P"
-    development = "D"
-    test        = "T"
-    integration = "I"
+    spielwiese   = "S"
+    spielwiese-p = "S"
+    production   = "P"
+    development  = "D"
+    test         = "T"
+    integration  = "I"
   }
 }
 
@@ -30,30 +32,56 @@ output "stage_letter" {
   value = var.stage_letter_map[var.stage_map[var.workspace]]
 }
 
-variable "syslog_ip_list_map" {
-  description = "List of fixed IPs for syslog instances"
+variable "searchhead_ip_list_map" {
+  description = "List of fixed IPs for searchhead instances"
   type = "map"
   default = {
-    spielwiese    = ["10.104.198.150",
-                     "10.104.198.182"]
+    spielwiese   = []
+    # if using own networks
+    #spielwiese   = ["10.0.1.20",
+    #                "10.0.2.20"]
+    spielwiese-p = ["10.104.146.225",
+                    "10.104.146.241"]
   }
 }
 
-output "syslog_ip_list" {
-  value = var.syslog_ip_list_map[var.stage_map[var.workspace]]
+output "searchhead_ip_list" {
+  value = var.searchhead_ip_list_map[var.stage_map[var.workspace]]
 }
 
 variable "indexer_ip_list_map" {
   description = "List of fixed IPs for indexer instances"
   type = "map"
   default = {
-    spielwiese = ["10.104.198.138",
-                  "10.104.198.171",
-                  "10.104.198.132",
-                  "10.104.198.169"]
+    spielwiese   = ["10.104.198.138",
+                    "10.104.198.171"]
+    # if using own networks
+    #spielwiese   = ["10.0.1.10",
+    #                "10.0.2.10"]
+    spielwiese-p = ["10.104.146.226",
+                    "10.104.146.242"]
   }
 }
 
 output "indexer_ip_list" {
   value = var.indexer_ip_list_map[var.stage_map[var.workspace]]
+}
+
+variable "syslog_ip_list_map" {
+  description = "List of fixed IPs for syslog instances"
+  type = "map"
+  default = {
+    # if using real IPs
+    spielwiese    = ["10.104.198.150",
+                     "10.104.198.182"]
+    # if using own networks
+    #spielwiese    = ["10.0.1.30",
+    #                 "10.0.2.30"]
+    spielwiese-p  = ["10.104.146.227",
+                     "10.104.146.243"]
+  }
+}
+
+output "syslog_ip_list" {
+  value = var.syslog_ip_list_map[var.stage_map[var.workspace]]
 }
