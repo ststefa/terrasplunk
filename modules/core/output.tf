7c4fba1752abd37fecb037c91b3823eee9eea169
Stefan Steinert <ststefa@heldenzeit.net>
2019-07-12 10:47:56 +0200

    makes network transparent to consumers

--- modules/core/output.tf
+++ modules/core/output.tf
@@ -1,45 +1,29 @@
 output "network1_id" {
-  value = openstack_networking_network_v2.core1.id
-}
-output "network2_id" {
-  value = openstack_networking_network_v2.core2.id
-}
-
-output "subnet1_id" {
-  value = openstack_networking_subnet_v2.core1.id
-}
-output "subnet2_id" {
-  value = openstack_networking_subnet_v2.core2.id
-}
-
-output "interface1" {
-  value = openstack_networking_router_interface_v2.core1
-}
-output "interface2" {
-  value = openstack_networking_router_interface_v2.core2
-}
-
-output "network_az1_id" {
+#  value = openstack_networking_network_v2.core1.id
   value = data.openstack_networking_network_v2.AppSvc_T_net_AZ1.id
 }
-output "network_az2_id" {
+output "network2_id" {
+#  value = openstack_networking_network_v2.core2.id
   value = data.openstack_networking_network_v2.AppSvc_T_net_AZ2.id
 }
 
-output "subnet_az1_id" {
+output "subnet1_id" {
+#  value = openstack_networking_subnet_v2.core1.id
   value = data.openstack_networking_subnet_v2.AppSvc_T_subnet_AZ1.id
 }
-output "subnet_az2_id" {
+output "subnet2_id" {
+#  value = openstack_networking_subnet_v2.core2.id
   value = data.openstack_networking_subnet_v2.AppSvc_T_subnet_AZ2.id
 }
 
-#output "interface_az1_id" {
-#  value = openstack_networking_router_interface_v2.AppSvc_T_net_AZ1.id
-#}
-#output "interface_az2_id" {
-#  value = openstack_networking_router_interface_v2.AppSvc_T_net_AZ2.id
-#}
-
+output "interface1" {
+#  value = openstack_networking_router_interface_v2.core1
+  value = ""
+}
+output "interface2" {
+#  value = openstack_networking_router_interface_v2.core2
+  value = ""
+}
 
 output "keypair_id" {
   value = openstack_compute_keypair_v2.keypair.id
