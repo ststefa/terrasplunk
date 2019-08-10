f6cb36c959fa5d4a881437d8d4659c190568f705
Stefan Steinert <ststefa@heldenzeit.net>
2019-08-10 10:43:11 +0200

    match output names

--- stages/spielwiese/output.tf
+++ stages/spielwiese/output.tf
@@ -1,31 +1,31 @@
 # outputs are just for testing pupose and are (currently) not referenced by anything. Might be referenced by provisioning later on. Alternatively provisioning might as well take values directly from terraform.tfstate files.
 
 output "indexer1_id" {
-  value = module.indexer1.id
+  value = module.server-ix00.id
 }
 output "indexer1_ip" {
-  value = module.indexer1.ip
+  value = module.server-ix00.ip
 }
 output "indexer1_name" {
-  value = module.indexer1.name
+  value = module.server-ix00.name
 }
 
 output "syslog1_id" {
-  value = module.syslog1.id
+  value = module.server-sy00.id
 }
 output "syslog1_ip" {
-  value = module.syslog1.ip
+  value = module.server-sy00.ip
 }
 output "syslog1_name" {
-  value = module.syslog1.name
+  value = module.server-sy00.name
 }
 
 output "searchhead1_id" {
-  value = module.searchhead1.id
+  value = module.server-sh00.id
 }
 output "searchhead1_ip" {
-  value = module.searchhead1.ip
+  value = module.server-sh00.ip
 }
 output "searchhead1_name" {
-  value = module.searchhead1.name
+  value = module.server-sh00.name
 }
