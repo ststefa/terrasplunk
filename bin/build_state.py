9f1aeeef6181cbb0a91a2d07d036e0b94aa1e3d1
Stefan Steinert <ststefa@heldenzeit.net>
2019-08-14 17:36:08 +0200

    dynamically collect stages based on terraform statefiles found

--- bin/build_state.py
+++ bin/build_state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import re
 import sys
 import argparse
 import json
@@ -10,7 +11,7 @@ import pathlib
 import logging
 
 # assign terraform workspace names to otc tenant names
-workspace_tenant_map = {
+workspace_tenant_map = { #TODO: remove if terraform.workspace==tenantname
     'default': {
         'tenant_name': 'tsch_rz_t_001',
         'tfstate_path': 'terraform.tfstate'
@@ -21,12 +22,6 @@ workspace_tenant_map = {
     }
 }
 
-# assign real stage names to terraform dir names
-stage_map = {'production': 'prod',
-             'qa': 'qa',
-             'test': 'test',
-             'development': 'dev',
-             'spielwiese': 'spielwiese'}
 
 log = logging.getLogger(__name__)
 # set to DEBUG for early-stage debugging
@@ -87,6 +82,9 @@ def main():
     # set our own main path (i.e. the parent path of the dir containing $0)
     base_path = os.path.normpath(
         os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
+    log.debug('base_path: %s' % base_path)
+    stage_path = os.path.join(base_path, 'stages')
+    shared_path = os.path.join(base_path, 'shared')
 
     parser = init_parser(base_path)
     args = parser.parse_args()
@@ -101,21 +99,28 @@ def main():
     log.debug('base_path: %s' % base_path)
 
     result = {}
+    reg_stagename = re.compile("..")
 
     for workspace in workspace_tenant_map.keys():
         tenant = {}
         result[workspace_tenant_map[workspace]['tenant_name']] = tenant
-        for stage_name in stage_map.keys():
-            stage_path = base_path + '/stages/' + stage_map[stage_name]
-            log.debug('stage_path: %s' % stage_path)
-            if not pathlib.Path(stage_path).exists():
-                raise TfstateError(
-                    'ERROR: Cannot find a directory for stage %s (%s), terraform structure incomplete' % (
-                    stage_name, stage_path))
 
-            tfstate_filename = stage_path + '/' + \
-                               workspace_tenant_map[workspace][
-                                   'tfstate_path']
+        # dynamically get stages based on existing dirs
+        stages = os.listdir(stage_path)
+        for stage in stages:
+            log.debug('stage: %s' % stage)
+            stagedir = os.path.join(base_path, stage)
+            if os.path.isdir(stagedir):
+                stages.remove(stage)
+            if not reg_stagename.fullmatch(stage):
+                stages.remove(stage)
+        log.debug('stages: %s' % stages)
+
+        for stage_name in stages:
+            this_stage_path = os.path.join(stage_path, stage_name)
+            log.debug('this_stage_path: %s' % this_stage_path)
+
+            tfstate_filename = os.path.join(this_stage_path, workspace_tenant_map[workspace]['tfstate_path'])
             log.debug('tfstate_filename: %s' % tfstate_filename)
             file = pathlib.Path(tfstate_filename)
             if file.exists():
