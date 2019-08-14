80f703844cf1e93fb9b2dba7ad7f45e4364d6258
Stefan Steinert <ststefa@heldenzeit.net>
2019-08-14 17:38:32 +0200

    doc

--- modules/core/main.tf
+++ modules/core/main.tf
@@ -4,3 +4,6 @@ locals {
 
 
 # Security group resources have been moved to shared/. See there for reasoning.
+
+# Nothing left here for now. Place resources/data here which are shared within
+# a stage on a tenant (but not among stages).
