d7a4f592e29c04085fdffb070ee3a0c4919bbf5d
Stefan Steinert <ststefa@heldenzeit.net>
2019-08-14 17:40:21 +0200

    using only name

--- modules/sh/input.tf
+++ modules/sh/input.tf
@@ -3,13 +3,6 @@
 #  default = "false"
 #}
 
-variable "stage" {
+variable "name" {
   description = "The stage (p0, t0, ...)"
-}
-variable "role" {
-  description = "The stage (ix, sh, ...)"
-}
-variable "number" {
-  type        = number
-  description = "Sequential number of the instance, starting with 0"
 }
