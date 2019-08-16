ad0ea319b64ef5be84ab062e8a7277438a34fbd9
Stefan Steinert <ststefa@heldenzeit.net>
2019-08-16 09:38:07 +0200

    fix and reorder outputs

--- shared/output.tf
+++ shared/output.tf
@@ -1,39 +1,44 @@
 output "netA-az1_id" {
   value = data.opentelekomcloud_networking_network_v2.netA-az1.id
 }
+output "subnetA-az1_id" {
+  value = opentelekomcloud_vpc_subnet_v1.subnetA-az1.id
+}
+
 output "netA-az2_id" {
   value = data.opentelekomcloud_networking_network_v2.netA-az2.id
 }
-#output "netB-az1_id" {
-#  value = data.opentelekomcloud_networking_network_v2.netB-az1.id
-#}
-#output "netB-az2_id" {
-#  value = data.opentelekomcloud_networking_network_v2.netB-az2.id
-#}
+output "subnetA-az2_id" {
+  value = opentelekomcloud_vpc_subnet_v1.subnetA-az2.id
+}
+
+output "netB-az1_id" {
+  count = local.tenant_name == "tsch_rz_p_001" ? 1 : 0
+  value = data.opentelekomcloud_networking_network_v2.netB-az1.id
+}
+output "subnetB-az1_id" {
+  count = local.tenant_name == "tsch_rz_p_001" ? 1 : 0
+  value = opentelekomcloud_vpc_subnet_v1.subnetB-az1.id
+}
+
+output "netB-az2_id" {
+  count = local.tenant_name == "tsch_rz_p_001" ? 1 : 0
+  value = data.opentelekomcloud_networking_network_v2.netB-az2.id
+}
+output "subnetB-az2_id" {
+  count = local.tenant_name == "tsch_rz_p_001" ? 1 : 0
+  value = opentelekomcloud_vpc_subnet_v1.subnetB-az2.id
+}
+
 output "netC-az1_id" {
   value = data.opentelekomcloud_networking_network_v2.netC-az1.id
 }
-output "netC-az2_id" {
-  value = data.opentelekomcloud_networking_network_v2.netC-az2.id
+output "subnetC-az1_id" {
+  value = opentelekomcloud_vpc_subnet_v1.subnetC-az1.id
 }
 
-output "subnetA-az1_id" {
-  #value = data.opentelekomcloud_vpc_subnet_v1.subnetA_az1.subnet_id
-  value = opentelekomcloud_vpc_subnet_v1.subnetA-az1.id
-}
-output "subnetA-az2_id" {
-  #value = data.opentelekomcloud_vpc_subnet_v1.subnetA_az2.subnet_id
-  value = opentelekomcloud_vpc_subnet_v1.subnetA-az2.id
-}
-#output "subnetB-az1_id" {
-#  value = data.opentelekomcloud_vpc_subnet_v1.subnetB_az1.subnet_id
-#}
-#output "subnetB-az2_id" {
-#  value = data.opentelekomcloud_vpc_subnet_v1.subnetB_az2.subnet_id
-#}
-output "subnetC-az1_id" {
-  #value = data.opentelekomcloud_vpc_subnet_v1.subnetC_az1.subnet_id
-  value = opentelekomcloud_vpc_subnet_v1.subnetC-az1.id
+output "netC-az2_id" {
+  value = data.opentelekomcloud_networking_network_v2.netC-az2.id
 }
 output "subnetC-az2_id" {
   #value = data.opentelekomcloud_vpc_subnet_v1.subnetC_az2.subnet_id
