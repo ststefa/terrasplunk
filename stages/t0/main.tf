969e179fe7ff6df0b9d19493e383540602798ab5
Stefan Steinert <stefan.steinert@t-systems.com>
2020-01-10 12:40:37 +0100

    add es001 to prod t0/p0

--- stages/t0/main.tf
+++ stages/t0/main.tf
@@ -56,6 +56,11 @@ module "server-sh002" {
   instance_name = "${local.prefix}sh002"
 }
 
+module "server-es001" {
+  source        = "../../modules/es"
+  instance_name = "${local.prefix}es001"
+}
+
 module "server-ix000" {
   source        = "../../modules/ix"
   instance_name = "${local.prefix}ix000"
