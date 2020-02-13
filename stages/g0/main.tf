9bb6f5fd2d2f8ab82af720d9802063a232b7dd05
Stefan Steinert <stefan.steinert@t-systems.com>
2020-02-13 19:36:49 +0100

    add g0 stage

---
+++ stages/g0/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_version = ">= 0.12.15"
+}
+
+locals {
+  stage  = basename(abspath("${path.root}"))
+  prefix = "spl${local.stage}"
+}
+
+provider "opentelekomcloud" {
+  domain_name = module.variables.tenant
+  tenant_name = "eu-ch_splunk"
+  user_name   = var.username
+  password    = var.password
+  #delegated_project = "eu-ch_splunk"
+  auth_url = "https://iam.eu-ch.o13bb.otc.t-systems.com/v3"
+}
+
+module "variables" {
+  source    = "../../modules/variables"
+  workspace = terraform.workspace
+  stage     = local.stage
+}
+
+module "core" {
+  source = "../../modules/core"
+  stage  = local.stage
+}
+
+data "terraform_remote_state" "shared" {
+  backend = "local"
+  config = {
+    path = module.variables.shared_statefile
+  }
+}
+
+module "server-ds000" {
+  source         = "../../modules/ds"
+  instance_name  = "${local.prefix}ds000"
+}
+module "server-lm000" {
+  source         = "../../modules/lm"
+  instance_name  = "${local.prefix}lm000"
+}
