b50fe74bd7ea50ead147026649119e18d205bdc6
Stefan Steinert <stefan.steinert@t-systems.com>
2020-05-08 18:48:31 +0200

    doc

--- modules/core/main.tf
+++ modules/core/main.tf
@@ -5,5 +5,8 @@ locals {
 
 # Security group resources have been moved to shared/. See there for reasoning.
 
-# Nothing left here for now. Place resources/data here which are shared within
-# a stage on a tenant (but not among stages).
+# Nothing left here for now. Place resources/data here which are shared among a
+# single stage in both tenants (but not among stages). In other words: This is
+# the place for stage-specific (but not tenant-specific) resources and data
+# blocks. Differences between stages can be encapsulated using the
+# variables module just in the same way as in the other modules.
