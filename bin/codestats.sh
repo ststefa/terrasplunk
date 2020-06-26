b87fea29d80af05ef47262c93945b9d7e637afe1
Stefan Steinert <stefan.steinert@t-systems.com>
2020-06-26 12:32:31 +0200

    update dev scripts

--- bin/codestats.sh
+++ bin/codestats.sh
@@ -6,17 +6,12 @@
 BASE_DIR="$(cd "$(dirname "$0")"/.. && pwd)" || exit "$(false)"
 CODE_DIRS="${BASE_DIR}/modules ${BASE_DIR}/shared ${BASE_DIR}/stages"
 
-echo "Lines of terraform code"
-for F in $(find ${CODE_DIRS} -name "*.tf") ; do
-    cat $F
-done | wc -l
+echo "Lines of terraform code (excluding comments and empty lines)"
+while read -r FILE ; do
+    cat "${FILE}" | grep -v '^\ *#' | grep -v '^\ *$'
+done < <(find ${CODE_DIRS} -name "*.tf") | wc -l
 
-echo "Lines of terraform code excluding comments"
-for F in $(find ${CODE_DIRS} -name "*.tf") ; do
-    cat $F | grep -v '^\ *#'
-done | wc -l
-
-echo "Lines of terraform code excluding comments and empty lines"
-for F in $(find ${CODE_DIRS} -name "*.tf") ; do
-    cat $F | grep -v '^\ *#' | grep -v '^\ *$'
-done | wc -l
+echo "Lines of comments in terraform code"
+while read -r FILE ; do
+    cat "${FILE}" | grep '^\ *#'
+done < <(find ${CODE_DIRS} -name "*.tf") | wc -l
