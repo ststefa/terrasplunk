c83c4b11751a196b8aa3e919034c254ac07a47cc
ue56070 <stefan.steinert@t-systems.com>
2020-07-01 14:03:50 +0200

    bin/codestats.sh edited online with Bitbucket

--- bin/codestats.sh
+++ bin/codestats.sh
@@ -8,7 +8,7 @@ CODE_DIRS="${BASE_DIR}/modules ${BASE_DIR}/shared ${BASE_DIR}/stages"
 
 echo "Lines of terraform code (excluding comments and empty lines)"
 while read -r FILE ; do
-    cat "${FILE}" | grep -v '^\ *#' | grep -v '^\ *$'
+    egrep -v '^\ *#|^\ *$' "${FILE}"
 done < <(find ${CODE_DIRS} -name "*.tf") | wc -l
 
 echo "Lines of comments in terraform code"
