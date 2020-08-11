f237c36d0883a978fb31dc2c91fa423dc3f90c54
Stefan Steinert <stefan.steinert@t-systems.com>
2020-08-11 12:50:23 +0200

    add some stats

--- bin/codestats.sh
+++ bin/codestats.sh
@@ -8,10 +8,25 @@ CODE_DIRS="${BASE_DIR}/modules ${BASE_DIR}/shared ${BASE_DIR}/stages"
 
 echo "Lines of terraform code (excluding comments and empty lines)"
 while read -r FILE ; do
-    egrep -v '^\ *#|^\ *$' "${FILE}"
+    grep -v '^\ *#|^\ *$' "${FILE}"
 done < <(find ${CODE_DIRS} -name "*.tf") | wc -l
 
 echo "Lines of comments in terraform code"
 while read -r FILE ; do
     grep '^\ *#' "${FILE}"
+done < <(find ${CODE_DIRS} -name "*.tf") | wc -l
+
+echo "Number of resource blocks"
+while read -r FILE ; do
+    grep '^resource \"' "${FILE}"
+done < <(find ${CODE_DIRS} -name "*.tf") | wc -l
+
+echo "Number of data blocks"
+while read -r FILE ; do
+    grep '^data \"' "${FILE}"
+done < <(find ${CODE_DIRS} -name "*.tf") | wc -l
+
+echo "Number of module blocks"
+while read -r FILE ; do
+    grep '^module \"' "${FILE}"
 done < <(find ${CODE_DIRS} -name "*.tf") | wc -l
